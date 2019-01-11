2851dde905d1111996fa1def6a4cf7eda9b4cc72
Sergey Malinin <sergmalinin@gmail.com>
2019-01-11 15:43:57 +1000

    Fx batch files

--- prepare_chrome.sh
+++ prepare_chrome.sh
@@ -20,16 +20,26 @@ SRC_DIR=$EXT_SRC
 DST_DIR=$EXT_DIRNAME
 
 #copy common files
-for I_DIR in background.html background.js options.html options.js popup.css popup.html popup.js settings.js webrequest.js ; do
+for I_DIR in OidcWebid.js background.html background.js certificate.js; do
   cp -va $SRC_DIR/$I_DIR $DST_DIR/
 done
 
+for I_DIR in content-script.js oidc-webid-inject.js options.css; do
+  cp -va $SRC_DIR/$I_DIR $DST_DIR/
+done
+
+for I_DIR in options.html options.js popup.css popup.html popup.js settings.js; do
+  cp -va $SRC_DIR/$I_DIR $DST_DIR/
+done
+
+for I_DIR in utils.js webrequest.js youid_view.js; do
+  cp -va $SRC_DIR/$I_DIR $DST_DIR/
+done
 
 cp -va $SRC_DIR/browser.js    $DST_DIR/
 cp -va $SRC_DIR/manifest.json $DST_DIR/
 
-
-for I_DIR in images lib; do
+for I_DIR in fonts images lib tpl; do
   mkdir -pv $DST_DIR/$I_DIR
   tar --exclude 'original' -cf - -C $SRC_DIR/$I_DIR .|tar -xf - -C $DST_DIR/$I_DIR
 done
